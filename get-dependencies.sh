eb58f415b5e3666a45cf5155e9c7336b051e2028
Link4Electronics <link4electronics@gmail.com>
2026-05-03 12:30:34 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,40 +6,34 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-    cmake    \
-    libdecor \
-    python   \
-    sdl2
+pacman -Syu --noconfirm cmake python-jinja sdl2-compat
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
-
-# If the application needs to be manually built that has to be done down here
 echo "Building OpenBoardView..."
 echo "---------------------------------------------------------------"
-git clone --recursive https://github.com/OpenBoardView/OpenBoardView ./OpenBoardView
-cd ./OpenBoardView && (
-	if [ "${DEVEL_RELEASE-}" = 1 ]; then
-		echo "Making nightly build of OpenBoardView..."
-		echo "---------------------------------------------------------------"
-		git rev-parse --short HEAD > ~/version
-	else
-		echo "Making stable build of OpenBoardView..."
-		echo "---------------------------------------------------------------"
-		TAG=$(git tag | grep -vi 'rc\|alpha\|^R' | sort -nr | head -1)
-		git checkout "$TAG"
-		echo "$TAG" > ~/version
-	fi
-	cmake -DCMAKE_BUILD_TYPE=Release ./
-	make -j"$(nproc)"
-	ls -lA ./
-	ls -lA ./src/openboardview
-)
+REPO="https://github.com/OpenBoardView/OpenBoardView"
+git clone --recursive --depth 1 "$REPO" ./OpenBoardView
 
 mkdir -p ./AppDir/bin
-mv -v ./OpenBoardView/src/openboardview/openboardview ./AppDir/bin
+cd ./OpenBoardView
+if [ "${DEVEL_RELEASE-}" = 1 ]; then
+	echo "Making nightly build of OpenBoardView..."
+	echo "---------------------------------------------------------------"
+	git rev-parse --short HEAD > ~/version
+else
+	echo "Making stable build of OpenBoardView..."
+	echo "---------------------------------------------------------------"
+	TAG=$(git tag | grep -vi 'rc\|alpha\|^R' | sort -nr | head -1)
+	if [ -z "$TAG" ]; then
+		TAG=$(git rev-parse --short HEAD)
+	else
+		git checkout "$TAG"
+	fi
+	echo "$TAG" > ~/version
+fi
+cmake -DCMAKE_BUILD_TYPE=Release ./
+make -j$(nproc)
+mv -v ./src/openboardview/openboardview ../AppDir/bin
