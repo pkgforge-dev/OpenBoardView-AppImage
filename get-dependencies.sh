571962fdd46e123bff18d8c4114acfb61baba4f0
Link4Electronics <link4electronics@gmail.com>
2026-08-23 20:01:14 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -8,13 +8,12 @@ echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
     cmake    	 \
-    libdecor 	 \
 	python-jinja \
     sdl2
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
 echo "Building OpenBoardView..."
 echo "---------------------------------------------------------------"
